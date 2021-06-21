619def4c473ed773edd2d3fb0163096413a923ec
abhandari095 <59588128+abhandari095@users.noreply.github.com>
2021-06-20 22:00:15 -0400

    Update accountsdb.sql

--- src/main/resources/accountsdb.sql
+++ src/main/resources/accountsdb.sql
@@ -3,7 +3,7 @@
 -- Host: localhost    Database: accounts
 -- ------------------------------------------------------
 -- Server version	5.7.18-0ubuntu0.16.10.1
-
+--Test
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
